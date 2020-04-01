67b99dc48f0b1345a03cd617492bbf5f1afe8724
KfWelch <kylef.welch@gmail.com>
2020-04-01 13:00:43 -0400

    Got the slider ones done

--- poke-compare/src/components/poke-menu-component/PokeMenuComponent.tsx
+++ poke-compare/src/components/poke-menu-component/PokeMenuComponent.tsx
@@ -5,6 +5,9 @@ import AbilitySelectComponent from '../poke-props-select-components/pokes-abilit
 import MoveSelectComponent from '../poke-props-select-components/pokes-move-select/MoveSelectContainer';
 import NatureSelectComponent from '../poke-props-select-components/pokes-nature-select/NatureSelectContainer';
 import PokeStatusComponent from '../poke-props-select-components/pokes-status-select/PokeStatusContainer';
+import HealthSelectComponent from '../poke-props-select-components/pokes-health-select/HealthSelectContainer';
+import LevelSelectComponent from '../poke-props-select-components/pokes-level-select/LevelSelectContainer';
+import HappySelectComponent from '../poke-props-select-components/pokes-happy-select/HappySelectContainer';
 
 interface IMenuProps {
     teamPokemon: any,
@@ -19,17 +22,23 @@ export class PokeMenuComponent extends React.Component<IMenuProps, any> {
             <Row>
                 <Col>
                     <PokemonSelectorComponent buttonText='Choose your Pokémon!' team="team"/>
+                    <LevelSelectComponent team="team" />
+                    <HealthSelectComponent team="team" />
                     <AbilitySelectComponent team="team"></AbilitySelectComponent>
                     <MoveSelectComponent team="team"/>
                     <NatureSelectComponent team="team"/>
                     <PokeStatusComponent team="team" />
+                    <HappySelectComponent team="team" />
                 </Col>
                 <Col>
                     <PokemonSelectorComponent buttonText='Choose opposing Pokémon!' team="opponent"/>
+                    <LevelSelectComponent team="opponent" />
+                    <HealthSelectComponent team="opponent" />
                     <AbilitySelectComponent team="opponent"></AbilitySelectComponent>
                     <MoveSelectComponent team="opponent"/>
                     <NatureSelectComponent team="opponent"/>
                     <PokeStatusComponent team="opponent" />
+                    <HappySelectComponent team="opponent" />
                 </Col>
             </Row>
             </>
---
+++ poke-compare/src/components/poke-props-select-components/pokes-level-select/LevelSelectComponent.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Slider } from '@material-ui/core';
+
+interface ILevelState {
+    level: any
+}
+
+interface ILevelProps {
+    team: string,
+    teamPokemon: any,
+    opponentPokemon: any,
+    teamModifiers: any,
+    opponentModifiers: any,
+    updateTeamLevel: (level: number) => void,
+    updateOpponentLevel: (level: number) => void
+}
+
+function valuetext(value: any) {
+    return `Level ${value}`;
+}
+
+export class LevelSelectComponent extends React.Component<ILevelProps, ILevelState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            level: 1
+        };
+    }
+
+    updateLevel = (event: any, value: any) => {
+        this.setState({
+            ...this.state,
+            level: value
+        })
+    }
+
+    updateStoreLevel = () => {
+        if (this.props.team === 'team') {
+            this.props.updateTeamLevel(this.state.level);
+        } else {
+            this.props.updateOpponentLevel(this.state.level);
+        }
+    }
+
+    
+
+    render() {
+        if ((this.props.team === 'team' && this.props.teamPokemon) || (this.props.opponentPokemon)) {
+            return (
+                <>
+                    <h4>Choose Pokémon Level</h4>
+                    <Slider
+                        defaultValue={1}
+                        getAriaValueText={valuetext}
+                        aria-labelledby="discrete-slider"
+                        valueLabelDisplay="auto"
+                        onChange={this.updateLevel}
+                        onMouseUp={this.updateStoreLevel}
+                        step={1}
+                        min={1}
+                        max={100}
+                    />
+                </>
+            )   
+        } else {
+            return (
+                <div/>
+            )
+        }
+    }
+}
